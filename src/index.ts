edb8bddf7413cdbfbc24e4f96e8a184b0ea80d53
William Sedlacek <wsedlacekc@gmail.com>
2019-10-29 13:46:10 -0500

    Add CORS
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import { postsRouter } from './routes/posts.route';
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 server.use('/api/posts', postsRouter);
 
 const port = 3000;
